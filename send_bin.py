8a46a8c7594f9974173b0174ee9d7b4b17eb53ba
Tore <torekbeyer@gmail.com>
2026-01-22 16:55:46 +0100

    bin folder

--- send_bin.py
+++ send_bin.py
@@ -3,34 +3,38 @@ import time
 import sys
 import os
 
-port = "COM3"
-baud = 9200
-filename = "VGA.bin"
-ser = serial.Serial(port, baudrate=baud, timeout=1)
-print(f"Opened {port} at {baud} baud")
-time.sleep(0.1)
+def send(name = "LED"):
+    port = "COM3"
+    baud = 9200
+    filename = "binaryFiles/"+ name + ".bin"
+    ser = serial.Serial(port, baudrate=baud, timeout=1)
+    print(f"Opened {port} at {baud} baud")
+    time.sleep(0.1)
 
-with open(filename, "rb") as f:
-    data = f.read()
+    with open(filename, "rb") as f:
+        data = f.read()
 
-#Pad to multiple of 4 bytes
-pad = (4 - (len(data) % 4)) % 4
-if pad:
-    data += b"\x00" * pad
+    #Pad to multiple of 4 bytes
+    pad = (4 - (len(data) % 4)) % 4
+    if pad:
+        data += b"\x00" * pad
 
-data += b"\x01\x01\x01\x01"
+    data += b"\x01\x01\x01\x01"
 
-size = len(data)
-print(f"Sending {size} bytes...")
-
-
-sent = 0
-for b in data:
-    ser.write(bytes([b]))
-    sent += 1
-    #print(f"{b:02x}")
-    time.sleep(0.005)
+    size = len(data)
+    print(f"Sending {size} bytes...")
 
 
-print(f"Done! Sent {sent} bytes.")
-ser.close()
+    sent = 0
+    for b in data:
+        ser.write(bytes([b]))
+        sent += 1
+        #print(f"{b:02x}")
+        time.sleep(0.005)
+
+
+    print(f"Done! Sent {sent} bytes.")
+    ser.close()
+
+if __name__ == "__main__":
+    send(sys.argv[1])
